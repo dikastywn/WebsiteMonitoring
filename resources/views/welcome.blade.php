350e4fe7358565e73337a64ba9df6ae4a57ea8fa
DikaSty <dikasetiawan062004@gmail.com>
2025-11-11 19:08:10 +0700

    tambahan halaman h1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <!-- TAMBAHAN DIKA-->
         <h1>Halaman Tambahan<h1>
 
         <!-- Fonts -->
